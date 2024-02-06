66d5b88c0611acc899af5a54ed17cf4601fd7652
kbaek98 <kbaek1@terpmail.umd.edu>
2024-02-05 20:17:52 -0500

    Test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World from Katie Baek! This is my first code change.'
+    return 'Hello World from Katie Baek. This is my first code change.'
 @app.route('/hello')
 def hello():
     return render_template('hello.html')
