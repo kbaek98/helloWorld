061efca2b2e7e926cd776d7f31b6ae6045337bec
kbaek98 <kbaek1@terpmail.umd.edu>
2024-03-25 21:10:39 -0400

    Created html files for base, contact, and favorite-course, and added routes for contact and favorite-course pages.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,8 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World from Katie Baek! This is my first code change.'
-@app.route('/hello')
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
+    print(f'First Name: {request.args.get("first_name")}')
+    print(f'Last Name: {request.args.get("last_name")}')
     return render_template('hello.html')
 
 @app.route('/about')
@@ -17,6 +19,19 @@ def about():
 @app.route('/about-css')
 def aboutcss():
     return render_template('about-css.html')
+
+@app.route('/favorite-course', methods=['GET', 'POST'])
+def favoritecourse():
+    print(f'Subject: {request.args.get("subject")}')
+    print(f'Course Number: {request.args.get("course_number")}')
+    return render_template('favorite-course.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run()
